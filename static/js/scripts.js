5d3682c11daa8e42a9f585d047ad68b68354d3d7
barbed-1122 <tzy1714925@163.com>
2025-11-14 12:41:17 +0800

    v2

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'scientific research project']
+const section_names = ['home', 'publications', 'project']
 
 
 window.addEventListener('DOMContentLoaded', event => {
